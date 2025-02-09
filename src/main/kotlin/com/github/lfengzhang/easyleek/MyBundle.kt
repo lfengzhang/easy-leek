c0196f3b40460f1c70d60612af22d1e2f645e0b6
GitHub Action <action@github.com>
2025-02-09 03:31:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/lfengzhang/easyleek/MyBundle.kt
+++ src/main/kotlin/com/github/lfengzhang/easyleek/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lfengzhang.easyleek
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lfengzhang/easyleek/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lfengzhang/easyleek/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lfengzhang.easyleek.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
